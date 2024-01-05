7481cf357cba045ee16cc02922160bd804267d06
AleksandrKostyuchenko <cheshirus91@gmail.com>
2024-01-05 15:55:16 +0300

    lesson11

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img className="img" src="https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/good-icon.png" alt="" />
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="" />
+        </div>
+        <div>avatar</div>
+        <div>my posts
+          <div>new posts</div>
+        </div>
+      </div>
+    );
+};
+
